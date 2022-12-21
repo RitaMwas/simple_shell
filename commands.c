64a1c0771f29de607ddd857a119368163160bbc1
programmersalomegathumbi <salomegathumbi@gmail.com>
2022-12-21 10:01:44 +0300

    betty fix

--- commands.c
+++ commands.c
@@ -1,18 +1,6 @@
 #include "shell.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * _strcat - ncatenates two strings
  *
  * @dest: Destination string
@@ -123,4 +111,3 @@ char *_strcpy(char *dest, char *src)
 	dest[i + 1] = 0;
 	return (dest);
 }
- 
