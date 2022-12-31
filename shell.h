367df354a23bdba46c9bf82c2c5332ba4ec2f2f7
Rita Mwangi <ritahmwangi@gmail.com>
2022-12-31 06:57:09 -0800

    Header file

--- shell.h
+++ shell.h
@@ -10,28 +10,92 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <signal.h>
+#include <limits.h>
+#include <fcntl.h>
 
-/* Environ global variable */
 extern char **environ;
-/* Function Prototypes */
-int lsh_ctrld(char **args);
-int lsh_cd(char **args);
-int lsh_exit(char **args);
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} env_t;
+
+/* main.c */
+int exec(char **input, char *s, int *i, env_t **head);
+
+/* path_finder.c */
+char **get_env(char *name, char **env);
+char *path_finder(char **s, char **env);
+char *get_env_val(char *name, char **env);
+
+/* tokenize.c */
+int wordcount(char *str, char delim);
+char **_strtok(char *str, char delim);
+
+/* print_funcs.c  */
+void print_prompt(void);
+int _putchar(char c);
+void _puts(char *str);
+
+/* print_errors.c */
+void print_error(int *i, char *s, char **argv);
+void print_error_env(char **argv);
+void print_error_exit(int *i, char *s, char **argv);
+void print_error_main(char **av);
+void print_error_cd(int *i, char *s, char **argv);
+
+/* string_funcs_1.c */
 int _strcmp(char *s1, char *s2);
-size_t _strncmp(char *s1, char *s2, size_t n);
+char *str_concat(char *s1, char *s2);
+char *_strstr(char *haystack, char *needle);
+int _strncmp(char *s1, char *s2, unsigned int n);
+char *_strdup(char *str);
+
+/* string_funcs_2.c */
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int _putchar(char c);
 
-char *_get_path(char **env);
-int _values_path(char **arg, char **env);
-char *_getline_command(void);
-void _getenv(char **env);
-char **tokenize(char *lineptr);
-void _exit_command(char **args, char *lineptr, int _exit);
-int _fork_fun(char **arg, char **av, char **env,
-char *lineptr, int np, int c);
+/* helper_funcs.c */
+void free_everything(char **args);
+void sigint_handler(int sig);
+char **parse_line(char *line, int get);
 
+/* builtins.c */
+int is_builtin(char *line, char **argv, char *prog_name, int *i, env_t **head);
+long int exit_handler(char **tokens);
+int env_handler(char **av, env_t **head);
+int cd_handler(char **argv, env_t **head);
+void change_pwd(char *path, char **env, env_t **head);
 
-#endif /* SHELL_H */
+/* convert.c */
+char *convert(int num, int base);
+long int _atoi(char *s);
+
+/* list_funcs_1.c */
+env_t *add_node_end(env_t **head, char *str);
+int add_node_at_index(env_t **head, char *str, int index);
+int delete_node_at_index(env_t **head, unsigned int index);
+int find_index_list(env_t *head, char *name);
+
+/* list_funcs_2.c */
+size_t list_len(const env_t *h);
+size_t print_list(env_t *h);
+void free_list(env_t **head);
+int arr_to_list(env_t **head, char **env);
+char **list_to_arr(env_t *head);
+
+/* set_env.c  */
+int _unsetenv(env_t **head, char **argv);
+int _setenv(env_t **head, char **argv, int args);
+void setenv_handler(char **argv, env_t **head, int *i, char *prog_name);
+void print_error_setenv(int *i, char *s, char **argv);
+
+#endif
